1f127a711b4199a741e4585490b6d0fefdf041ce
Bryan Harris <bryanharris49@gmail.com>
2018-05-21 01:49:56 -0400

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            string userInput;
+            bool appRunning = true;
+            
+            Console.WriteLine("Bryan's Stopwatch");
+            while (appRunning == true)
+            {
+                Console.WriteLine("Press s to start the watch; st to stop the watch; press r to reset stopwatch; press e to exit");
+                userInput = Console.ReadLine();
+                switch (userInput.ToLower())
+                {
+                    case "s":
+                        try
+                        {
+                            stopwatch.Start();
+                            Console.WriteLine("Stopwatch started...");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+                    case "st":
+                        try
+                        {
+                            stopwatch.Stop();
+                            Console.WriteLine("Stopwatch stopped...");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+                    case "e":
+                        try
+                        {
+                            appRunning = false;
+                            Console.WriteLine("Stopwatch stopped...");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+                    case "r":
+                        try
+                        {
+                            stopwatch.Reset();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Error! Please press a button listed above");
+                        break;
+                }
+
+
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private TimeSpan _duration;
+        private bool _isRunning = false;
+
+        public TimeSpan TotalDuration
+        {
+            get
+            {
+                return _duration;
+            }
+            set { _duration += value; }
+        }
+        
+  
+
+
+        public void Start()
+        {
+            if (_isRunning == false)
+            {
+                _startTime = DateTime.Now;
+                _isRunning = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("Clock already running...");
+            }
+
+        }
+
+        public void Stop()
+        {
+            if (_isRunning == true)
+            {
+                _stopTime = DateTime.Now;
+                _isRunning = false;
+                TotalDuration = _stopTime - _startTime;
+                Console.WriteLine("Time elapsed: {0}", TotalDuration);
+                
+            }
+            else
+            {
+                throw new InvalidOperationException("Clock already stopped");
+            }
+        }
+
+        public void Reset()
+        {
+            _duration = TimeSpan.Zero;
+            _startTime = DateTime.MinValue;
+            _stopTime = DateTime.MinValue;
+            _isRunning = false;
+
+            Console.WriteLine(TotalDuration);
+            Console.WriteLine("Time reset");
+        }
+
+        
+    }
+}
